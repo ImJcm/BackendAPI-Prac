da3c18dc359743fb406c40c8c2d4f34f9e8c6bc7
ImJcm <wjsckdals0909@naver.com>
2023-07-25 19:52:26 +0900

    Initial commit

---
+++ src/main/java/com/example/backendapiprac/BackendApiPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendapiprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendapiprac/BackendApiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendapiprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
